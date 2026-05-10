38df42dbfbdd68fd3e7b638b947f514b50b4900a
Webtick SA <webticksa@gmail.com>
2026-05-10 14:10:08 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouterVite({
+      codeSplittingOptions: {
+        addHmr: false,
+      },
+    }),
     tanstackStart({
       server: { entry: 'src/server.ts' }
     }),
