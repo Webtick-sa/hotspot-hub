3db85d17b088e08c2d1b99703e24dd38c0fcbbdd
Webtick SA <webticksa@gmail.com>
2026-05-13 00:15:01 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		RouterProvider router={router} />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
